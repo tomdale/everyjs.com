02b7bcc205c0963dac06244e024b52ae2afac587
Tom Dale <tom@sproutcore.com>
2011-05-29 18:35:32 -0700

    Fill in preliminary "use if" data. We want to be able to help people disambiguate between all of the options.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@ EveryJS.libraries = [
     name: "jQuery",
     website: "http://www.jquery.com",
     description: "jQuery is a fast and concise JavaScript library that simplifies HTML document traversing, event handling, animating, and Ajax interactions for rapid web development.",
-    useIf: [ "You need DOM manipulation or event handling across all browsers." ],
+    useIf: [
+      "You need DOM manipulation or event handling across all browsers."
+    ],
     dependencies: null,
     license: "MIT",
     tags: ['dom'],
@@ -16,7 +18,9 @@ EveryJS.libraries = [
     name: "Ender",
     website: "http://ender.no.de/",
     description: "Ender is an open, powerful, micro-to-macro API for composing your own custom JavaScript library; it wraps up application agnostic, independent modules into a slick, intuitive, and familiar interface so you don't have to.",
-    useIf: [ "You need DOM manipulation or event handling across all browsers." ],
+    useIf: [
+      "You want to easily construct your own bespoke framework."
+    ],
     size: "7.5K with default configuration. Will vary"
   },
 
@@ -24,7 +28,10 @@ EveryJS.libraries = [
     name: "SproutCore",
     website: "http://www.sproutcore.com",
     description: "SproutCore provides both an MVC architecture and bindings, with views that automatically update any time properties change.",
-    useIf: [ "You need DOM manipulation or event handling across all browsers." ],
+    useIf: [
+      "You want to avoid bugs related to keeping the DOM in sync with your data model.",
+      "You want stateful MVC, like you'd find in Cocoa."
+    ],
     dependencies: ["jQuery"],
     size: 29
   },
@@ -33,7 +40,6 @@ EveryJS.libraries = [
     name: "Backbone.js",
     website: "http://documentcloud.github.com/backbone/",
     description: "Backbone supplies structure to JavaScript-heavy applications by providing models with key-value binding and custom events, collections with a rich API of enumerable functions, views with declarative event handling, and connects it all to your existing application over a RESTful JSON interface.",
-    useIf: [ "You need DOM manipulation or event handling across all browsers." ],
     dependencies: ["Underscore", "jQuery or Zepto (optional but recommended)"],
     size: 3.9
   },
@@ -58,6 +64,9 @@ EveryJS.libraries = [
     name: "Morpheus",
     website: "https://github.com/ded/morpheus",
     description: "Morpheus lets you \"tween anything\" in parallel on multiple elements, from integers to colors, with easing transitions, in a single high-performant loop utilizing the CPU-friendly requestAnimationFrame standard.",
+    useIf: [
+      "You want to animate multiple elements and have them stay in sync."
+    ],
     size: 1.3
   },
 
@@ -66,7 +75,9 @@ EveryJS.libraries = [
     website: "http://xuijs.com/",
     description: "A super micro tiny DOM library for authoring HTML5 mobile web applications.",
     browserSupport: "Different builds for WebKit, BlackBerry, and IE.",
-    useIf: [ "You need DOM manipulation or event handling across all browsers." ],
+    useIf: [
+      "You are able to deliver different libraries depending on the platform, such as in PhoneGap builds."
+    ],
     size: "8K or lower, depending on build"
   },
 
@@ -75,6 +86,9 @@ EveryJS.libraries = [
     website: "http://zeptojs.com/",
     description: "Zepto.js is a minimalist JavaScript framework for mobile WebKit browsers, with a jQuery-compatible syntax.",
     browserSupport: "Supports mobile WebKit browsers only.",
+    useIf: [
+      "You don't have to support desktop browsers and file size is important."
+    ],
     size: 4.8
   },
 
